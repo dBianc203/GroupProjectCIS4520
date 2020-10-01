2c85bf1a26a46212d64cb0d950107c60fd40d7fc
Devon Biancarelli <62315100+dBianc203@users.noreply.github.com>
2020-10-01 19:36:44 -0400

    Add files via upload

--- Java Folder/BudgetProj.java
+++ Java Folder/BudgetProj.java
@@ -10,13 +10,20 @@ import java.util.Scanner;
 
 public class BudgetProj
 {
+    /** Scanner for user input */
+    public static Scanner scan = new Scanner(System.in);
+    /** double that holds the users monthly budget */
+    public static double UserBudget;
+    /** double that holds the input made by the user that represents a purchase made */
+    public static double UserExpense;
+    /** have the total expenses made by a user = to 0 */
+    public static double TotalExpenses = 0;
+    /** if users input is "y" the program will ask the user to enter a new expense */
+    public static char MoreExpenses = 'y';
+    
     public static void main (String [] args) 
     {
-        Scanner scan = new Scanner(System.in);
-        double UserBudget;
-        double UserExpense;
-        double TotalExpenses = 0;
-        char MoreExpenses = 'y';
+
         
         System.out.println("How much do you want to budget for this month?");
         UserBudget = scan.nextDouble();
