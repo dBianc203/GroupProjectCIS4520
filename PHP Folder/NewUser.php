462530c91317c179aed47fb8cc365efbb1ba652a
rjm07270 <69855335+rjm07270@users.noreply.github.com>
2020-11-02 20:28:49 -0500

    Fixed wronge error messages to user and rm error check

--- PHP Folder/NewUser.php
+++ PHP Folder/NewUser.php
@@ -4,12 +4,89 @@ include('connect.php');
 $errors =array('Email'=>'','Password'=>'','LastName'=>'','FirstName'=>'','Age'=>'','SocialSecurity'=>'','Address1'=>'','Address2'=>'');
 //!saves users input when submit is clicked
 $Email = $Password = $LastName = $FirstName = $Age = $SocialSecurity = $Address1 = $Address2 = '';
-//checks errors
-include('error_check.php');
+
+if(isset($_POST['submit'])){
+  if (empty($_POST['Email'])) {
+    $errors['Email'] = "Email is required";
+  }else {
+    $Email = $_POST['Email'];
+    //!vaild email check
+    if (!filter_var($Email, FILTER_VALIDATE_EMAIL)) {
+      $errors['Email'] =  "Email must be valid";
+    }
+  }
+
+  //!vaild Password check
+  if (empty($_POST['Password'])) {
+    $errors['Password'] = "Password is required";
+  }else {
+    $Password=$_POST['Password'];
+    if(!preg_match('/^[a-zA-Z1-9]{9,255}$/',$Password)){
+    $errors['Password'] =  " Enter a valid password with a lenth of above 9 and no special characters";
+  }
+  }
+  //!vaild LastName check
+  if (empty($_POST['LastName'])) {
+    $errors['LastName'] = "Last Name is required";
+  }else {
+    $LastName=$_POST['LastName'];
+    if(!preg_match('/^[a-zA-Z1-9]{1,255}$/',$LastName)){
+  $errors['LastName'] =  " Enter a valid Last Name with no special characters";
+  }
+}
+
+  //!vaild FirstName
+  if (empty($_POST['FirstName'])) {
+    $errors['FirstName'] =  "Last Name is required";
+  }else {
+    $FirstName=$_POST['FirstName'];
+    if(!preg_match('/^[a-zA-Z1-9]{1,255}$/', $LastName)){
+    $errors['FirstName'] =  " Enter a valid First Name with no special characters";
+  }
+  }
+
+  //!vaild Age check
+  if (empty($_POST['Age'])) {
+    $errors['Age'] =  "Age is required";
+  }else {
+    $Age=$_POST['Age'];
+    if(!preg_match('/^[1]?[1-9][1-9]$/',$Age)){
+    $errors['Age'] =  " Enter a valid Age";
+  }
+  }
+
+  //!vaild SocialSecurity check
+  if (empty($_POST['SocialSecurity'])) {
+    $errors['SocialSecurity'] =  "Social Security is required";
+  }else {
+    $SocialSecurity=$_POST['SocialSecurity'];
+    if(!preg_match('/^[a-zA-Z1-9]{9,255}$/',$SocialSecurity)){
+    $errors['SocialSecurity'] =  " Enter a valid Social Security with a lenth of above 9 and no special characters";
+  }
+  }
+
+  //!vaild Address1 check
+  if (empty($_POST['Address1'])) {
+    $errors['Address1'] =  "Address1 is required";
+  }else {
+    $Address1=$_POST['Address1'];
+    if(!preg_match('/^[a-zA-Z1-9\s]{1,255}$/', $Address1)){
+    $errors['Address1'] =  " Enter a valid Address1 with a lenth of above 9 and no special characters";
+  }
+  }
+
+  //!vaild Address2 check
+    if (!empty($_POST['Address2'])){
+      $Address2=$_POST['Address2'];
+      if(!preg_match('/^[a-zA-Z1-9]{1,255}$/', $Address2)){
+        $errors['Address2'] =  " Enter a valid Address1 with a lenth of above 1 and no special chachars or space";
+      }
+  }
+
 
 
   //!Puts user data in database if their are no errors
-  if(count($errors)<1){
+  if($errors['Email']==""and$errors['Password']==""and$errors['LastName']==""and$errors['FirstName']==""and$errors['Age']==""and$errors['SocialSecurity']==""and$errors['Address1']==""){
     //! Inserting each string into database
     $Email = mysqli_real_escape_string($conn, $_POST['Email']);
      $Eassword = mysqli_real_escape_string($conn, $_POST['Password']);
@@ -22,14 +99,12 @@ include('error_check.php');
      $sql = "INSERT INTO people(Email, Password, LastName, FirstName, Age, SocialSecurity, Address1, Address2)
             VALUES('$Email', '$Password', '$LastName', '$FirstName', '$Age', '$SocialSecurity', '$Address1', '$Address2')";
      if(mysqli_query($conn,$sql)){
-       echo "Submited";
+       echo "Hell yha";
      }else{
        echo "sql error ".mysqli_error($conn);
      }
   }
 }
-$conn-->close();
-
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -37,32 +112,6 @@ $conn-->close();
     <meta charset="utf-8">
     <title>The Bank</title>
   </head>
-<style>
-  input[type=text],
-  input[type=password],
-  input[type=number]
-
-
-  {
-      width: 100%;
-      padding: 12px 20px;
-      margin: 8px 0;
-      display: inline-block;
-      border: 1px solid #ccc;
-      box-sizing: border-box;
-  }
-  input[type=submit]
-  {
-      background-color: #4CAF50;
-      color: white;
-      padding: 14px 20px;
-      margin: 8px 0;
-      border: none;
-      cursor: pointer;
-      width: 100%;
-  }
-</style>
-
   <body>
     <section>
       <h4 class="center"></h4>
@@ -100,13 +149,13 @@ $conn-->close();
 
         <!--get address1-->
         <label for="">Address1:</label><br>
-        <input type="text" name="Address1" value= <?php echo $Address1 ?>><br>
+        <input type="text" name="Address1" value= "<?php echo $Address1 ?>"><br>
         <div class="red-text"><?php echo $errors['Address1']; ?></div>
 
 
 	<!--get address2-->
         <label for="">Address2:</label><br>
-        <input type="text" name="Address2" value= <?php echo $Address2 ?>><br>
+        <input type="text" name="Address2" value= "<?php echo $Address2 ?>"><br>
         <div class="red-text"><?php echo $errors['Address2']; ?></div>
 
           <input input = "submit" type="submit" name="submit" value="submit">
