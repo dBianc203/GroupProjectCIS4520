07a933b752a5a2ff6ec6f500199d975d5bfc617d
Devon Biancarelli <62315100+dBianc203@users.noreply.github.com>
2020-11-15 04:50:07 -0500

    User is given acc bal. and can depo/With/and Trans

--- PHP Folder/SuccDepositInSave.php
+++ PHP Folder/SuccDepositInSave.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +11,19 @@
 <body>
   <div class="Question">
     You successfully deposited <?php echo number_format($_POST["DepoAmount"]); ?> into your savings account
+
+<?php 				
+ $first_number = $_POST["DepoAmount"];
+
+ $Background_number = $_SESSION["SavingsAccount"];
+ 
+ 
+ $new_balance = (($Background_number)+($first_number));
+ echo 'Your new balance is ';
+ print ($new_balance);
+ $_SESSION["SavingsAccount"] = ($new_balance);
+
+	?>
 </div>
 <div class="btn-group1">
   <a href="ATM Front page.html"><button>Continue to main screen</button>
