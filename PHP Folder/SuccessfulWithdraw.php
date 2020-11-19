c0eca5dae3a7c2341b172272e59fd8651c306f0b
Devon Biancarelli <62315100+dBianc203@users.noreply.github.com>
2020-11-19 03:08:21 -0500

    prevent negative withdraw

--- PHP Folder/SuccessfulWithdraw.php
+++ PHP Folder/SuccessfulWithdraw.php
@@ -8,19 +8,25 @@ session_start();
 </head>
 <body>
   <div class="Question">
- You successfully withdrew <?php echo number_format ($_POST["QuickDraw"]); ?> from your account
+ You attempted to withdraw <?php echo number_format ($_POST["QuickDraw"]); ?> from your account <br>
 
  <?php 				
  $first_number = $_POST["QuickDraw"];
 
  $Background_number = $_SESSION["CheckingAccount"];
- 
- 
+  if ($Background_number < 20){
+	 echo 'You failed to withdraw from this account (negative balance will occur)';
+ }
+ else {
  $new_balance = (($Background_number)-($first_number));
+  if ($new_balance < 0)
+ {
+	 $new_balance = 0;
+ }
  echo 'Your new balance is ';
  print ($new_balance);
  $_SESSION["CheckingAccount"] = ($new_balance);
-
+ }
 	?>
 </div>
 <div class="btn-group1">
