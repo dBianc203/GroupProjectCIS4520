beb5d4053962c3b87f47197c9f1adfe92034c4a0
rjm07270 <69855335+rjm07270@users.noreply.github.com>
2020-11-25 19:32:07 -0500

    missing bracket and added error corection for no seshions

--- PHP Folder/timer.php
+++ PHP Folder/timer.php
@@ -2,13 +2,16 @@
 session_start();
 $sessionID=$_GET['$sessionID'];
 $sql = "SELECT  ID, endPT, startPT  FROM sechions where sessionID='$sessionID' ORDER BY startPT DESC";
+if(mysqli_query($conn,$sql)){
+  $result = $conn->query($sql);
 
-while($row = $result->fetch_assoc()) {
-  $endPT=$row["endPT"];
-  $startPT=$row["endPT"];
-  $ID=$row["ID"];
+ if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+      $endPT=$row["endPT"];
+      $startPT=$row["endPT"];
+      $ID=$row["ID"];
 
-}
+      }
      $_SESSION['sessionID']=$sessionID;
 if($sessionID==null||(!($endPT>time() && $startPT<time()))){
   $_SESSION['sessionID']=$sessionID;
@@ -17,6 +20,11 @@ exit();
 }else {
   $sql = "INSERT INTO sessions(ID,endPT,startPT)
          VALUES($ID,ADDTIME(CAST(CURTIME() AS TIME),1800),CAST(CURTIME() AS TIME))";
+     }
+    } else {
+echo "0 results";
 }
-
+}else{
+  echo "sql error ".mysqli_error($conn);
+}
  ?>
